ce66587799e8426c24192f20a6b2c345d4e4a4d3
Hack Reactor Students <github@hackreactor.com>
2013-10-24 11:20:47 -0700

    adds time/date formatting and handles links that have never been visited

--- public/client/linkView.js
+++ public/client/linkView.js
@@ -5,7 +5,7 @@ Shortly.LinkView = Backbone.View.extend({
   template: _.template(' \
       <img src="/redirect_icon.png"/> \
       <div class="info"> \
-        <div class="visits"><span class="count"><%= visits %></span>Visits.  Last on: <%= last_visited %> </div> \
+        <div class="visits"><span class="count"><%= visits %></span>Visits.  Last on: <%= function(){ if(last_visited === null){ return "never";}else{return new Date(last_visited).toDateString() + " at "+new Date(last_visited).toLocaleTimeString();}}() %></div> \
         <div class="title"><%= title %></div> \
         <div class="original"><%= url %></div> \
         <a href="<%= base_url %>/<%= code %>"><%= base_url %>/<%= code %></a> <a href="<%= base_url %>/<%= code %>/stats">(stats)</a> \

16a57323e1c369d76396e56081dcbefbf9e47c12
Hack Reactor Students <github@hackreactor.com>
2013-10-23 20:27:26 -0700

    works on links

--- public/client/links.js
+++ public/client/links.js
@@ -3,16 +3,18 @@ Shortly.Links = Backbone.Collection.extend({
   model: Shortly.Link,
   url: '/links',
 
+  sortStrategy: 'visitsDESC',
+
   comparator: function(item){
-    return -item.get('visits');
+    return this.sortStrategies[this.sortStrategy](item);
+  },
+
+  sortStrategies: {
+    visitsDESC: function(item){
+      return -item.get('visits');
+    },
+    visitsASC: function(item){
+      return item.get('visits');
+    }
   }
-
-  // strategies: {
-  //   mostVistited: function(){
-
-  //   },
-  //   lastVisited: function(){
-
-  //   }
-  // }
 });
